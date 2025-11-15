988f8ebe8822dc24ba69ef8009ba144b3bb2442e
cheeseburgertony <1821955020@qq.com>
2025-11-16 00:22:11 +0800

    feat: 添加ai后台任务支持

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,3 +1,4 @@
+import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 // 创建一个 Inngest 函数，监听 "test/hello.world" 事件
@@ -7,11 +8,20 @@ export const helloWorld = inngest.createFunction(
   { event: "test/hello.world" }, // 监听的事件
   // 处理程序
   async ({ event, step }) => {
-    // 模拟第一个异步操作10s
-    await step.sleep("wait-a-moment", "10s");
-    // 模拟第二个异步操作5s
-    await step.sleep("almost-done", "5s");
-    // 返回结果
-    return { message: `Hello ${event.data.email}!` };
+    const summarizer = createAgent({
+      model: openai({
+        model: "deepseek-chat",
+        baseUrl: "https://api.deepseek.com/v1",
+      }),
+      name: "code-agent",
+      system:
+        "You are an expert next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets.",
+    });
+
+    const { output } = await summarizer.run(
+      `Write the following snippet: ${event.data.value}`
+    );
+
+    return { output };
   }
 );
