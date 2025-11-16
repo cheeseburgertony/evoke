bf61913f91c2b461735a2c05efae7e6ff66c3089
cheeseburgertony <1821955020@qq.com>
2025-11-17 00:23:28 +0800

    feat: 添加工具调用生命周期支持，处理AI回复并保存任务摘要

--- src/inngest/utils.ts
+++ src/inngest/utils.ts
@@ -1,6 +1,23 @@
 import { Sandbox } from "@e2b/code-interpreter";
+import { AgentResult, TextMessage } from "@inngest/agent-kit";
 
 export async function getSandbox(sandboxId: string) {
   const sandbox = await Sandbox.connect(sandboxId);
   return sandbox;
 }
+
+// 获取AI回复的最新的文本内容
+export const lastAIMessageTextContent = (result: AgentResult) => {
+  const lastAIMessageIndex = result.output.findLastIndex(
+    (message) => message.role === "assistant"
+  );
+
+  const messgae = result.output[lastAIMessageIndex] as TextMessage | undefined;
+
+  if (messgae?.content) {
+    return typeof messgae.content === "string"
+      ? messgae.content
+      : messgae.content.map((c) => c.text).join("");
+  }
+  return undefined;
+};
