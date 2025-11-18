4bafa847d1fe26c3b6441a0670f49d630540ec8f
cheeseburgertony <1821955020@qq.com>
2025-11-18 16:16:56 +0800

    feat: 添加项目数据持久化，支持项目创建和对话消息关联

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,35 @@
 "use client";
 
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
-import { useMutation, useQuery } from "@tanstack/react-query";
-import { useState } from "react";
-import { toast } from "sonner";
 
 const Page = () => {
   const [value, setValue] = useState("");
 
   const trpc = useTRPC();
-  const { data: message } = useQuery(trpc.message.getMany.queryOptions());
-  const createMessage = useMutation(
-    trpc.message.create.mutationOptions({
-      onSuccess: () => {
-        toast.success("消息已发送");
+  const createProject = useMutation(
+    trpc.project.create.mutationOptions({
+      onError: (error) => {
+        toast.error(error.message);
       },
     })
   );
 
   return (
-    <div className="p-4 mx-auto max-w-7xl">
-      <Input value={value} onChange={(e) => setValue(e.target.value)} />
-      <Button
-        disabled={createMessage.isPending}
-        onClick={() => createMessage.mutate({ value })}
-      >
-        触发后台任务
-      </Button>
-      {JSON.stringify(message)}
+    <div className="h-screen w-screen flex items-center justify-center ">
+      <div className="max-w-7xl mx-auto flex flex-col justify-center items-center gap-y-4">
+        <Input value={value} onChange={(e) => setValue(e.target.value)} />
+        <Button
+          disabled={createProject.isPending}
+          onClick={() => createProject.mutate({ value })}
+        >
+          发送
+        </Button>
+      </div>
     </div>
   );
 };
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -198,6 +198,7 @@ export const codeAgentFunction = inngest.createFunction(
       if (isError) {
         return await prisma.message.create({
           data: {
+            projectId: event.data.projectId,
             content: "出现了一些错误，请再试一次。",
             role: "ASSISTANT",
             type: "ERROR",
@@ -207,6 +208,7 @@ export const codeAgentFunction = inngest.createFunction(
 
       return await prisma.message.create({
         data: {
+          projectId: event.data.projectId,
           content: result.state.data.summary,
           role: "ASSISTANT",
           type: "RESULT",
