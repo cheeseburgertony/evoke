4d0be911e7843e8ff542c308ee9258fd77f57e33
cheeseburgertony <1821955020@qq.com>
2025-11-22 17:41:41 +0800

    feat: 添加文件面包屑组件以支持文件路径导航

--- src/components/file-explorer/index.tsx
+++ src/components/file-explorer/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useCallback, useMemo, useState } from "react";
-import { CopyIcon } from "lucide-react";
+import { CopyCheckIcon, CopyIcon } from "lucide-react";
 import {
   ResizableHandle,
   ResizablePanel,
@@ -12,6 +12,7 @@ import { Button } from "../ui/button";
 import { CodeView } from "../code-view";
 import { TreeView } from "./c-cpns/tree-view";
 import { convertFilesToTreeItems } from "@/lib/utils";
+import { FileBreadcrumb } from "./c-cpns/file-breadcrumb";
 
 type FileCollection = { [path: string]: string };
 
@@ -30,6 +31,7 @@ interface IFileExplorerProps {
 }
 
 export const FileExplorer = ({ files }: IFileExplorerProps) => {
+  const [copied, setCopied] = useState(false);
   const [selectedFile, setSelectedFile] = useState<string | null>(() => {
     const fileKeys = Object.keys(files);
     return fileKeys.length > 0 ? fileKeys[0] : null;
@@ -48,6 +50,18 @@ export const FileExplorer = ({ files }: IFileExplorerProps) => {
     [files]
   );
 
+  const handleCopy = useCallback(() => {
+    if (selectedFile) {
+      try {
+        navigator.clipboard.writeText(files[selectedFile]);
+        setCopied(true);
+        setTimeout(() => setCopied(false), 2000);
+      } catch (err) {
+        console.error("复制错误: ", err);
+      }
+    }
+  }, [selectedFile, files]);
+
   return (
     <ResizablePanelGroup direction="horizontal">
       <ResizablePanel defaultSize={30} minSize={30} className="bg-sidebar">
@@ -62,16 +76,16 @@ export const FileExplorer = ({ files }: IFileExplorerProps) => {
         {selectedFile && files[selectedFile] ? (
           <div className="h-full w-full flex flex-col">
             <div className="border-b bg-sidebar px-4 py-2 flex justify-between items-center gap-x-2">
-              {/* TODO: 面包屑导航 */}
+              <FileBreadcrumb filePath={selectedFile} />
               <Hint text="复制到剪贴板" side="bottom">
                 <Button
                   variant="outline"
                   size="sm"
                   className="ml-auto"
-                  onClick={() => {}}
-                  disabled={false}
+                  onClick={handleCopy}
+                  disabled={copied}
                 >
-                  <CopyIcon />
+                  {copied ? <CopyCheckIcon /> : <CopyIcon />}
                 </Button>
               </Hint>
             </div>
