3f47d5565a66999c8136141ef59530a5c5f802d8
cheeseburgertony <1821955020@qq.com>
2025-11-22 15:06:00 +0800

    feat: 添加代码高亮组件

---
+++ src/components/code-view/index.tsx
@@ -0,0 +1,25 @@
+import Prism from "prismjs";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-jsx";
+import "prismjs/components/prism-tsx";
+import "prismjs/components/prism-css";
+import "prismjs/components/prism-typescript";
+import "./code-theme.css";
+import { useEffect } from "react";
+
+interface ICodeViewProps {
+  code: string;
+  lang: string;
+}
+
+export const CodeView = ({ code, lang }: ICodeViewProps) => {
+  useEffect(() => {
+    Prism.highlightAll();
+  }, [code]);
+
+  return (
+    <pre className="p-2 bg-transparent border-none rounded-none m-0 text-xs">
+      <code className={`language-${lang}`}>{code}</code>
+    </pre>
+  );
+};
