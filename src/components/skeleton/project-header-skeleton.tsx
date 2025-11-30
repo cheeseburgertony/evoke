e7d75f058eb2271131e4f6bcc2d9faf3d531f50a
cheeseburgertony <1821955020@qq.com>
2025-11-30 17:42:54 +0800

    fix: 优化错误边界组件的复制功能和项目头部骨架组件的布局

--- src/components/skeleton/project-header-skeleton.tsx
+++ src/components/skeleton/project-header-skeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export const ProjectHeaderSkeleton = () => {
   return (
-    <div className="p-2 flex justify-between items-center border-b">
+    <div className="p-2 flex items-center border-b">
       <Skeleton className="h-8 w-32" />
     </div>
   );
